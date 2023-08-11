3e1b57b5e943cf8c88158a68697acf56130f9e85
Gerutro <131986676+Gerutro@users.noreply.github.com>
2023-08-11 16:01:12 +0300

    Update main.py

--- code/cogs/main.py
+++ code/cogs/main.py
@@ -8,10 +8,11 @@ class main(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-  #cog listener to print in terminal status
+  #cog listener to print status in terminal
   @commands.Cog.listener()
   async def on_ready(self):
-    print('main cog - ACTIVATED')
+    print('main cog - ACTIVATED\n')
 
+#setup cog file
 def setup(bot):
   bot.add_cog(main(bot))

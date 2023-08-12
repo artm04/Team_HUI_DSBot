5c0b9cb54acb0cc29d09dad287ccc7a2b40c4417
Gerutro <131986676+Gerutro@users.noreply.github.com>
2023-08-12 11:34:59 +0300

    Update admin.py

--- code/cogs/admin.py
+++ code/cogs/admin.py
@@ -1,1 +1,19 @@
+#imports
+from disnake.ext import commands
+import disnake
+import sys
 
+
+#class create
+class admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    #cog listener to print status in terminal
+    @commands.Cog.listener()
+    async def on_ready(self):
+      print('admin cog - ACTIVATED\n')
+
+#setup cog file
+def setup(bot):
+    bot.add_cog(admin(bot))

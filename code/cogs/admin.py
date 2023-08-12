2155da2859411fef13de4d8d1b48b24732f145a8
Максим <131986676+Gerutro@users.noreply.github.com>
2023-08-12 21:22:47 +0300

    Update admin.py

--- code/cogs/admin.py
+++ code/cogs/admin.py
@@ -1,19 +1,50 @@
-#imports
-from disnake.ext import commands
+#Imports
 import disnake
-import sys
+from disnake.ext import commands
+import asyncio
 
 
-#class create
-class admin(commands.Cog):
+#Class create
+class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #cog listener to print status in terminal
+    #Cog listener
     @commands.Cog.listener()
     async def on_ready(self):
-      print('admin cog - ACTIVATED\n')
+        print("Admin cog - Online.")
 
-#setup cog file
+    #Clear chat
+    @commands.has_permissions(manage_messages=True)
+    @commands.command(name="clear")
+    async def clear_command(self, ctx, amount: int):
+        try:
+            channel = ctx.channel
+            messages = await channel.history(limit=amount+1).flatten()
+            await channel.delete_messages(messages)
+        except Exception as e:
+        	await ctx.send(f"Error: {e}")
+
+    #Kick members
+    @commands.has_permissions(kick_members=True)
+    @commands.slash_command(name="kick", description="Kick member")
+    async def kick(self, ctx, member: disnake.Member, reason="You have been excluded."):
+        try:
+            await member.kick(reason=reason)
+            await ctx.send(f"User {member} excluded, reason: {reason}", delete_after=10)
+        except Exception as e:
+           	await ctx.send(f"Error: {e}")
+
+    #Ban members
+    @commands.has_permissions(manage_member=True)
+    @commands.command(name="ban", description="Ban member")
+    async def ban(self, ctx, member: disnake.Member, reason="You have been ban."):
+        try:
+            await member.ban(reason=reason)
+            await ctx.send(f"User {member} ban, reason: {reason}", delete_after=10)
+        except Exception as e:
+            await ctx.send(f"Error: {e}")
+
+#Setup cog file
 def setup(bot):
-    bot.add_cog(admin(bot))
+    bot.add_cog(Admin(bot))

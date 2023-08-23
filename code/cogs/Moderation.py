c0a50ce92a80674fe9f71c903ceddb88e1a003df
Gerutro <gerutrogame@gmail.com>
2023-08-23 19:01:12 +0300

    gitignore, config, mofderation

--- code/cogs/Moderation.py
+++ code/cogs/Moderation.py
@@ -1,11 +1,16 @@
 #Imports
 from disnake.ext import commands
 import disnake
+import json
+
+with open('../config.json') as f:
+	banwords = json.load(f)
 
 
 #Last message var.
 last_messages = {}
-
+#Ban Words var.
+banwords_var = []
 
 #Class create
 class Moderation(commands.Cog):
@@ -28,7 +33,21 @@ class Moderation(commands.Cog):
 		last_messages[message.author.id] = message
 		
 		await self.bot.process_commands(message)
+
+	# Cog listener on_message again
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.author.id in last_messages:
+			if message.content == banwords_var[message.author.id].content:
+				await message.delete()
+				pass
+
+		await self.bot.process_commands(message)
+
+
+	for banwords_var in banwords:
+
 	
-#Setup cog filel
+#Setup cog file
 def setup(bot):
 	bot.add_cog(Moderation(bot))
